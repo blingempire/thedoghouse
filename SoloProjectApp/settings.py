c3bfdd0de9f1f580d931eaf7e2345073f491fcf4
blingempire <plat4m@outlook.com>
2021-06-11 12:23:31 -0700

    Update settings.py

--- SoloProjectApp/settings.py
+++ SoloProjectApp/settings.py
@@ -137,7 +137,8 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(STATICFILES_DIRS, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
