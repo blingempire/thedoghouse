ad3d8bdc73483613ba7d55a827d14e185a7c7f0d
blingempire <plat4m@outlook.com>
2021-06-11 12:07:23 -0700

    Update settings.py

--- SoloProjectApp/settings.py
+++ SoloProjectApp/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
