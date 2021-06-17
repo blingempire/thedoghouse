682738948f1f150bc60990929133ecf5281ce233
blingempire <plat4m@outlook.com>
2021-06-17 15:35:52 -0700

    Update settings.py

--- SoloProjectApp/settings.py
+++ SoloProjectApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wl3)#&r&swpv_0ec($ot7h21zkgf2bul6tx5^zu8$jru6u3@9_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
